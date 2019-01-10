613b5a2c3ab370b900719f7dd59e6c36544b1abf
y-tian <t.tian4321@gmail.com>
2019-01-10 17:17:12 -0500

    linkedlist

--- Main.py
+++ Main.py
@@ -1,22 +1,35 @@
 import logging
 from hashtable import HashLogic
+from linkedlists.singlylinkedlist import SLinkedList
+from linkedlists.singlylinkedlist import Node
 
 class Logic:
     def __init__(self):
         self.size = 50
         self.hastable = [0]*self.size
 
-    def testdata(self):
+    def testHashData(self):
         testString = ["abcdef", "bacdef", "cbadef", "defabc"]
         hashd = HashLogic(self.hastable, self.size)
         hashd.Insert(testString)
         hashd.PrintHashTable()
 
+    def testSLinkedListData(self):
+        nodeHead = Node("0")
+        linkedlist = SLinkedList()
+        linkedlist.head = nodeHead
+        linkedlist.appendNode("5")
+        linkedlist.insertNode("6", 1)
+        linkedlist.removeNode(2)
+        linkedlist.pushNode("2")
+        linkedlist.traverse()
+
 def main():
     Log = logging.getLogger('myLogger')
     Log.setLevel(20)
     logic = Logic()
-    logic.testdata()
+    # logic.testHashData()
+    logic.testSLinkedListData()
 
 main()
 
--- linkedlists/singlylinkedlist.py
+++ linkedlists/singlylinkedlist.py
@@ -3,10 +3,52 @@ import logging
 class Node:
     def __init__(self, data=None):
         self.data = data
-        self.nextNode = Node
+        self.nextNode = None
 
 class SLinkedList:
     def __init__(self):
         self.head = None
 
+    def traverse(self):
+        currNode = self.head
+        while currNode is not None:
+            logging.info(currNode.data)
+            print(currNode.data)
+            currNode = currNode.nextNode
 
+    def getSize(self):
+        size = 0
+        currNode = self.head
+        while currNode is not None:
+            size += 1
+            currNode = currNode.nextNode
+        return size
+
+    def appendNode(self, data):
+        head = self.head
+        newNode = Node(data)
+        head.nextNode = newNode
+    
+    def pushNode(self, data):
+        tempNode = self.head
+        newNode = Node(data)
+        self.head = newNode
+        self.head.nextNode = tempNode
+
+    def insertNode(self, data, index):
+        if index <= self.getSize():
+            newNode = Node(data)
+            currNode = self.head
+            for i in range(0, index-1):
+                currNode = currNode.nextNode
+            tempNode = currNode.nextNode
+            currNode.nextNode = newNode
+            currNode = currNode.nextNode
+            currNode.nextNode = tempNode
+    
+    def removeNode(self, index):
+        if index <= self.getSize():
+            currNode = self.head
+            for i in range(0, index-1):
+                currNode = currNode.nextNode
+            currNode.nextNode = currNode.nextNode.nextNode
